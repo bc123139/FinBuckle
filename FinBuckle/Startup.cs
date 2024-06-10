e06ef08d0ebd9a759260226e79aefed83cc34352
Usman Tahir <Usman.tahir@synergy-it.com>
2024-06-11 00:03:14 +0500

    change in appsettings

--- FinBuckle/Startup.cs
+++ FinBuckle/Startup.cs
@@ -44,10 +44,11 @@ namespace FinBuckleApi
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FinBuckle v1"));
+               
             }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FinBuckle v1"));
 
             app.UseHttpsRedirection();
             app.UseRouting();
