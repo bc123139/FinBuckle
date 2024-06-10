6438e059ba30a294f3fadc920c7df67d9d0193d3
Usman Tahir <Usman.tahir@synergy-it.com>
2024-06-10 23:38:04 +0500

    change in home service

--- FinBuckleMvc/Services/HomeService.cs
+++ FinBuckleMvc/Services/HomeService.cs
@@ -1,5 +1,6 @@
 ﻿using Finbuckle.MultiTenant;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -9,17 +10,19 @@ namespace FinBuckleMvc.Services
     {
         private readonly HttpClient _apiClient;
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public HomeService(HttpClient apiClient, IHttpContextAccessor httpContextAccessor)
+        private readonly IConfiguration _configuration;
+        public HomeService(HttpClient apiClient, IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
         {
             _apiClient = apiClient;
             _httpContextAccessor = httpContextAccessor;
+            _configuration = configuration;
         }
         public async Task<string> GetResult()
         {
             ITenantInfo tenantInfo = _httpContextAccessor.HttpContext?.GetMultiTenantContext<AppTenantInfo>()?.TenantInfo;
             try
             {
-                var dataString = await _apiClient.GetStringAsync($"https://localhost:5102/{tenantInfo.Identifier}/Home/GetResult");
+                var dataString = await _apiClient.GetStringAsync($"{_configuration["FinBuckleApi"]}/{tenantInfo.Identifier}/Home/GetResult");
                 return dataString;
             }
             catch (System.Exception ex)
